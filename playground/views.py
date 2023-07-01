3d3a2b6de104e3564f9ebeaa870d1dc6685beff6
HarshithRajesh <harshithr2004@gmail.com>
2023-07-01 12:56:01 +0530

    filtering objects

--- playground/views.py
+++ playground/views.py
@@ -4,9 +4,5 @@ from store.models import Product
 from django.core.exceptions import ObjectDoesNotExist
 
 def say_hello(request):
-
-    try:
-        product = Product.objects.get(pk=0)
-    except ObjectDoesNotExist:
-        pass
-    return render(request,"hello.html")
+    queryset = Product.objects.filter(unit_price__range=(20,30))
+    return render(request,"hello.html",{'name':'Ha','products':list(queryset)})
