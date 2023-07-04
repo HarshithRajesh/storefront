bc4e9a8d51088ce76dca918383e2e6dee1467107
HarshithRajesh <harshithr2004@gmail.com>
2023-07-04 20:59:32 +0530

    random commit

--- playground/views.py
+++ playground/views.py
@@ -6,3 +6,4 @@ from django.core.exceptions import ObjectDoesNotExist
 def say_hello(request):
     queryset = Product.objects.filter(unit_price__range=(20,30))
     return render(request,"hello.html",{'name':'Ha','products':list(queryset)})
+#random
