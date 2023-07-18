30d2f49b88e6f9c4472ef39b89da0329adb747e1
HarshithRajesh <harshithr2004@gmail.com>
2023-07-18 21:47:24 +0530

    overiding queryset

--- store/admin.py
+++ store/admin.py
@@ -1,10 +1,37 @@
+from typing import Any
 from django.contrib import admin
+from django.db.models.query import QuerySet
+from django.http.request import HttpRequest
 from . import models
-
+from django.db.models.aggregates import Count
+from django.utils.html import format_html
 @admin.register(models.Product)
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ['title','unit_price']
+    list_display = ['title','unit_price','inventory_status','collection_title']
     list_editable = ['unit_price']
+    list_per_page = 10
+    list_select_related =['collection']
+
+    def collection_title(self, product):
+        return product.collection.title
+
+    @admin.display(ordering='inventory')
+    def inventory_status(self, product):
+        if product.inventory < 10:
+            return 'Low'
+        return 'OK'
+@admin.register(models.Collection)
+class CollectionAdmin(admin.ModelAdmin):
+    list_display = ['title','products_count']
+
+    @admin.display(ordering='products_count')
+    def products_count(self, collection):
+        return collection.products_count()
+
+    def get_queryset(self, request):
+        return super().get_queryset(request).annotate(
+            products_count = Count('product')
+        )
 
 @admin.register(models.Customer)
 class CustomerAdmin(admin.ModelAdmin):
@@ -12,5 +39,8 @@ class CustomerAdmin(admin.ModelAdmin):
     list_editable = ['membership']
     ordering = ['first_name']
     list_per_page = 10
-admin.site.register(models.Collection)
-#random
+
+
+@admin.register(models.Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display =['id','placed_at','customer']
